4e74a07393472de8c83dd8dcf1fec5139ac3f9e9
Folahan <seunfola1@gmail.com>
2023-04-11 09:04:07 +0100

    model an id

--- Note_App/note.js
+++ Note_App/note.js
@@ -1,50 +1,90 @@
 #!/usr/bin/env node
 
 const yargs = require("yargs");
+yargs.version("v1.0.0-alpha");
 
-yargs.version ("v1.0.0-alpha");
+const fs = require("fs");
+const path = require("path");
+
+const noteJsonDir = path.resolve(__dirname, "./note.json")
 
 yargs.command({
     command: "add",
-    describe:"Add a new Note",
-    builder:{
-    title:{
-       describe: "Note Title",
-       demandOption: true,
-       type: "strings" 
-        }, 
-        body:{
-       describe: "The Note Body",
-       demandOption: true,
-       type: "strings" 
-        },   
+    describe: "Add a new Note",
+    builder: {
+        id: {
+            describe: "note Id",
+            demandOption: true,
+            type: "number"
+        },
+        title: {
+            describe: "Note Title",
+            demandOption: true,
+            type: "strings"
+        },
+        body: {
+            describe: "The Note Body",
+            demandOption: true,
+            type: "strings"
+        },
     },
-    handler: function(argv){
+    handler: function (argv) {
+        console.log(argv.id);
         console.log(argv.title);
-        console.log(argv.body);
-    } ,   
-    });
+        console.log(argv.body)
+        // console.log(`successfully deleted ${title}`); 
 
-    yargs.command({
-    command: "Remove",
-    describe:"Remove Note",
-    builder:{
-    title:{
-       describe: "Note Title",
-       demandOption: true,
-       type: "strings" 
-        }, 
-         
+        const noteJson = fs.readFileSync(noteJsonDir, "utf-8");
+        const notes = JSON.parse(noteJson);
+        const newNotes = {
+            id: argv.id,
+            title: argv.title,
+            body: argv.body
+        }
+        notes.push(newNotes)
+        const updateNotes = JSON.stringify(notes);
+        fs.writeFileSync(noteJsonDir, updateNotes);
+        console.log(`successfully added ${argv.id}`);
+        console.log(`successfully added ${argv.title}`)
     },
-    handler: function(argv){
-        const title =argv.title
-console.log(`successfully deleted ${title}`);       
-    } ,   
-    });
-    
-    yargs.parse()
-    
-    
-    
+});
+
+yargs.command({
+    command: "Remove",
+    describe: "Remove Note",
+    builder: {
+        title: {
+            describe: "Note Title",
+            demandOption: true,
+            type: "strings"
+        },
+
+    },
+    handler: function (argv) {
+        console.log(argv.title);
+        // console.log(argv.body);
+        note.filter((notes) => { notes.title !== title })
+        // console.log(`successfully deleted ${title}`); 
+
+        const noteJson = fs.readFileSync(noteJsonDir, "utf-8");
+        const notes = JSON.parse(noteJson);
+        const newNotes = {
+            id: argv.id,
+            title: argv.title,
+            body: argv.body
+        }
+        notes.push(newNotes)
+        const updateNotes = JSON.stringify(notes);
+        fs.writeFileSync(noteJsonDir, updateNotes);
+        console.log(`successfully added ${argv.id}`)
+        console.log(`successfully added ${argv.title}`)
+    }
+
+});
+
+yargs.parse()
+
+
+
 
 
--- playground/working_with_json.js
+++ playground/working_with_json.js
@@ -10,11 +10,13 @@ const note = JSON.parse(noteJson)
 console.log(note);
 
 const newNote = {
+    "id": 7,
     "title": "My seventh note",
-        "body":"This is my seventh Note"
-};const newNote2 = {
+    "body": "This is my seventh Note"
+}; const newNote2 = {
+    "id": 8,
     "title": "My eighth note",
-        "body":"This is my eight Note"
+    "body": "This is my eight Note"
 };
 
 
@@ -22,8 +24,8 @@ note.push(newNote);
 note.push(newNote2)
 console.log(note);
 
-note.map((note)=>{
-    console.log(i, note.title,note.body)
+note.map((note) => {
+    console.log(i, note.id, note.title, note.body)
 })
 const noteReady = JSON.stringify(note)
 
